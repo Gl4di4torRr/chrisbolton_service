c2f43558b84442d4b8995b454dc715b45ebb5325
Chris <Bolton@XATLMAC00070s-MacBook-Pro.local>
2017-09-13 19:18:08 -0400

    add cors

--- src/main/java/com/chrisbolton/chrisbolton_service/ChrisboltonServiceApplication.java
+++ src/main/java/com/chrisbolton/chrisbolton_service/ChrisboltonServiceApplication.java
@@ -6,23 +6,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @SpringBootApplication
 @RestController
 public class ChrisboltonServiceApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ChrisboltonServiceApplication.class, args);
 	}
 	
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
-		
+	
 	@RequestMapping(path="/blogs")
 	public @ResponseBody Iterable<ChrisBolton> getAllUsers() {
+		System.out.println("GET BLOGS");
 		List<ChrisBolton> result = jdbcTemplate.query(
                 "SELECT * FROM blog",
                 (rs, rowNum) -> new ChrisBolton(rs.getString("author"), 
@@ -33,4 +36,5 @@ public class ChrisboltonServiceApplication {
 
         return result;
 	}
+	
 }
